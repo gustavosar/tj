adce4ccb980bc94dfdd60956edb6541a85137f79
Gustavo <gustavo.sa@spassu.com>
2020-01-06 17:50:25 -0300

    PRIMENG E HTML CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,ViewChild } from '@angular/core';
+import {MenuItem,TreeNode} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+
+    filesTree: TreeNode[];
+
+    ngOnInit() {
+      this.filesTree = [
+        {
+          label: "Documents for implement",
+          data: "Documents Folder",
+          expandedIcon: "fa fa-folder-open",
+          collapsedIcon: "fa fa-folder",
+          children: [{
+                  label: "Work",
+                  data: "Work Folder",
+                  expandedIcon: "fa fa-folder-open",
+                  collapsedIcon: "fa fa-folder",
+                  children: [{"label": "Expenses.doc", "icon": "fa fa-file-word-o", "data": "Expenses Document"}, {"label": "Resume.doc", "icon": "fa fa-file-word-o", "data": "Resume Document"}]
+              },
+              {
+                  label: "Home",
+                  data: "Home Folder",
+                  expandedIcon: "fa fa-folder-open",
+                  collapsedIcon: "fa fa-folder",
+                  children: [{"label": "Invoices.txt", "icon": "fa fa-file-word-o", "data": "Invoices for this month"}]
+              }]
+      },
+
+      ];
+    }
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import {TreeModule} from 'primeng/tree';
-import {TreeNode} from 'primeng/api';
-import { TreeComponent } from './tree/tree.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TreeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TreeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
